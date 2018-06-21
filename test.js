d2919673094b7f495b632198a2344a7d0b12e2b7
Adam Swayze <adamswayze@adams-imac.lan>
2018-06-21 08:08:13 -0400

    Added replace all and columns

--- test.js
+++ test.js
@@ -1,11 +1,25 @@
+
+//Add your columns here:
+
+var columns = ['column1', 'column2', 'column3', 'column4'];
+
+
+
+
+
+
 function updateSQLCode(code){
-    var myKeyWords = ['SELECT', 'FROM', 'WHERE', 'WITH', 'AND', 'OR', 'GROUP BY', ''];
+    var myKeyWords = ['SELECT', 'FROM', 'WHERE', 'WITH', 'AND', 'OR', 'GROUP BY', 'DISTINCT'];
     myKeyWords.map((word) => {
-        code = code.replace(word, '<span class="keyWord">' + word + '</span>')
+        code = code.replace(new RegExp(word, 'g'), '<span class="keyWord">' + word + '</span>')
     })
     var SQLfunctions = ['MAX', 'MIN', 'COUNT']
     SQLfunctions.map((word) => {
-        code = code.replace(word, '<span class="SQLfunctions">' + word + '</span>')
+        code = code.replace(new RegExp(word, 'g'), '<span class="SQLfunctions">' + word + '</span>')
+    })
+
+    columns.map((word) => {
+        code = code.replace(new RegExp(word, 'g'), '<span class="tableColumns">' + word + '</span>')
     })
     return code
 }
